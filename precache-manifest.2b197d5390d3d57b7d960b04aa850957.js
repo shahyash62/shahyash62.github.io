2873b0f4843920b7e1190172d7d9ff78e1a47e44
shahyash62 <shahyash62@gmail.com>
2020-11-05 17:45:21 -0500

    Updates

---
+++ precache-manifest.2b197d5390d3d57b7d960b04aa850957.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9d3dc2be565acd136c73640112e7730d",
+    "url": "/shahyash62.github.io/index.html"
+  },
+  {
+    "revision": "5269683f87f9873ed3cc",
+    "url": "/shahyash62.github.io/static/css/main.2415b295.chunk.css"
+  },
+  {
+    "revision": "6d294a67a1d5dd9752dc",
+    "url": "/shahyash62.github.io/static/js/2.d1d175d5.chunk.js"
+  },
+  {
+    "revision": "09b9d3f0d51489787422ebbb109d049a",
+    "url": "/shahyash62.github.io/static/js/2.d1d175d5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5269683f87f9873ed3cc",
+    "url": "/shahyash62.github.io/static/js/main.ddbe681c.chunk.js"
+  },
+  {
+    "revision": "8e0158910b343e8b0970",
+    "url": "/shahyash62.github.io/static/js/runtime-main.53ef76b9.js"
+  },
+  {
+    "revision": "2462950488e5603319cee4e581eb50f9",
+    "url": "/shahyash62.github.io/static/media/resume.24629504.pdf"
+  },
+  {
+    "revision": "31f36fa91a672d6a4a1ab8f73a12501d",
+    "url": "/shahyash62.github.io/static/media/screenshot.31f36fa9.jpg"
+  },
+  {
+    "revision": "e7658b27dbcba5c222de88f9da9a4ccb",
+    "url": "/shahyash62.github.io/static/media/screenshot2.e7658b27.jpg"
+  },
+  {
+    "revision": "8244c5d09dc10fe1a28bde80ff284c31",
+    "url": "/shahyash62.github.io/static/media/screenshot3.8244c5d0.jpg"
+  },
+  {
+    "revision": "c745d9db383b34a633530b48db3eb63c",
+    "url": "/shahyash62.github.io/static/media/screenshot4.c745d9db.jpg"
+  }
+]);
